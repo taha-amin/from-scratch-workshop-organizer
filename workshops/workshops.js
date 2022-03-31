4c74e46578085f65dbf86a0f1c95586006b6a745
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-31 11:45:26 -0700

    added correct file path on line 39 of workshops/index.html. started writing displayWorkshops func in workshops.js

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -1,9 +1,17 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, deleteParticipant, getWorkshops, logout } from '../fetch-utils.js';
 
 checkAuth();
 
+const workshopsEl = document.querySelector('.workshops-container');
 const logoutButton = document.getElementById('logout');
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
+
+function displayWorkshops(workshops) {
+    //clear out workshopsEl
+    workshopsEl.textContent = '';
+
+    
+}
